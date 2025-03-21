0b5562a8b283096eb4a5b4b08e437f243dfcdeeb
Carlos Polo <carlos.polo@globant.com>
2025-03-21 09:14:45 -0500

    yml and gradle modified to handle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.carlospolo.melichallenge"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = System.getenv("VERSION_CODE")?.toInt() ?: 1
+        versionName = System.getenv("VERSION_NAME") ?: "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
